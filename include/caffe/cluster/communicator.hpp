fb0c47608c4df25561f772ed4781f9f8a0368735
Jian Zhang <zhangjianthu@gmail.com>
2016-07-14 21:45:49 -0700

    clique synchronization in progress

--- include/caffe/cluster/communicator.hpp
+++ include/caffe/cluster/communicator.hpp
@@ -1,3 +1,8 @@
+#ifndef COMMUNICATOR_HPP_
+#define COMMUNICATOR_HPP_
+
+#include <cstddef>
+
 /**
  * Derive or set differently for different communication  
  */
@@ -5,22 +10,36 @@
 template<typename Dtype>
 class CommConfig {
 public:
-  CommConfig(device_id, machine_id) :
+  CommConfig(int device_id, int machine_id, 
+    int n_dev_clique_local, int clique_id, int clique_rank) :
     device_id_(device_id), 
     machine_id_(machine_id),
+    n_dev_clique_local_(n_dev_clique_local),
+    clique_id_(clique_id),
+    clique_rank_(clique_rank),
     buffer_(NULL),
     left_gpu_buffer_(NULL), 
-    right_gpu_buffer_(NULL) {}
-  CommConfig(CommConfig& config) : 
-    device_id_(config.device_id_), machine_id_(config.machine_id_),
+    right_gpu_buffer_(NULL),
+    ncclComm_(NULL) {}
+  CommConfig(const CommConfig<Dtype>& config) : 
+    device_id_(config.device_id_), 
+    machine_id_(config.machine_id_),
+    n_dev_clique_local_(config.n_dev_clique_local_),
+    clique_id_(config.clique_id_),
+    clique_rank_(config.clique_rank_),
+    buffer_(config.buffer_),
     left_gpu_buffer_(config.left_gpu_buffer_),
-    right_gpu_buffer_(config.right_gpu_buffer_) {}
+    right_gpu_buffer_(config.right_gpu_buffer_),
+    ncclComm_(NULL) {}
   ~CommConfig() {}
-  inline int GetDeviceId() { return device_id; }
-  inline int GetMachineId() { return machine_id; } 
-  inline Dtype* GetBuffer() { return buffer_; }
-  inline Dtype* SetLeftGpuBuffer() { return left_gpu_buffer_; }
-  inline Dtype* SetRightGpuBuffer() { return right_gpu_buffer_; }
+
+  inline int GetDeviceId() { return device_id_; }
+  inline int GetMachineId() { return machine_id_; } 
+  Dtype** GetBufferPtrAddr() { return &buffer_; }
+  Dtype* GetBuffer() {return buffer_; }
+  void BufferMalloc(int64_t buf_size);
+  void SetLeftGpuBuffer(Dtype* buffer, int64_t buf_size);
+  void SetRightGpuBuffer(Dtype* buffer, int64_t buf_size);  
 private:
   /**
    * Communicator.SyncIntraMachine uses sync_gpu_ids_ to sync
@@ -33,6 +52,14 @@ private:
    */
   int device_id_;
   int machine_id_;
+  /* define how many dev on the local machine is in the same clique*/
+  int n_dev_clique_local_;
+  /**
+   * clique id specifies the GPU group in which current GPU is in.
+   * clique_rank specifies the device rank in the clique.
+   */
+  int clique_id_;
+  int clique_rank_;
   /**
    * buffer on the current, left and right machine for ring-base 
    * GPU communications.
@@ -40,6 +67,10 @@ private:
   Dtype* buffer_;
   Dtype* left_gpu_buffer_;
   Dtype* right_gpu_buffer_;
+  int64_t buf_size_;
+  int64_t left_buf_size_;
+  int64_t right_buf_size_;
+  ncclComm_t* nccl_comm_;
 }; 
 
 /*base communicator class*/
@@ -48,20 +79,29 @@ class Communicator {
 public:
   Communicator(CommConfig<Dtype>& config) : config_(config) {}
   ~Communicator() {}
-  /* Building blocks for different synchronization setting */
+  /**
+   * Building blocks for different synchronization setting
+   * Group may include gpus on multiple nodes. We call the 
+   * part of a group as the local clique on a node. 
+   */
+  virtual int SyncClique();
   virtual int SyncGroup();
   virtual int SyncIntraMachine();
-  virtual int SyncInterMachine();
-  /* communication primitives */
-  virtual int SendToPointInterMachine();
-  virtual int SendToPointIntraMachine();
-  virtual int AllReduceInterMachine();
-  virtual int AllReduceIntraMachine();
-  virtual int BroadCastIntraMachine();
-  /* Behaviors in computing process */
-  virtual int OnStart();
-  virtual int OnGradientReady();
+  // virtual int SyncInterMachine();
+  // /* communication primitives */
+  // virtual int SendToPointInterMachine();
+  // virtual int SendToPointIntraMachine();
+  // virtual int AllReduceInterMachine();
+  // virtual int AllReduceIntraMachine();
+  // virtual int BroadCastIntraMachine();
+  // /* Behaviors in computing process */
+  // virtual int OnStart();
+  // virtual int OnGradientReady();
 private:  
   /* configuration */
-  CommConfig config_;
+  CommConfig<Dtype> config_;
+
+friend Communicator<Dtype>;
 };
+
+#endif  // COMMUNICATOR_HPP_
