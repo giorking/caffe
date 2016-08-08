02b607f55955420f9b5f57200546860c0da4bc90
Jian Zhang <zhangjianthu@gmail.com>
2016-08-08 14:48:44 -0700

    for test on server

---
+++ include/cluster/comm_utils.hpp
@@ -0,0 +1,92 @@
+#ifndef COMM_UTILS_HPP_
+#define COMM_UTILS_HPP_
+
+/**
+ * In our communication protocal, each process spawns multiple threads.
+ * Each thread controls one GPU device. A group refers to multiple processes
+ * working synchronously. Each group has a root, which is the only thread in
+ * the group dealing with inter-group communication (asynchrony). A group 
+ * consists of multiple clique. A Clique is the part of group on 
+ * a single machine. More intitively, a process corresponds to a
+ * clique. The root thread of the clique deals with communication among
+ * multiple node in the group. In our protocol, we assume if a thread is
+ * group_root, it is also guaranteed to be a clique root. This avoids addtional
+ * communication between inter and intra operations.
+ */
+
+
+/* for definition of NULL */
+#include <cstddef>
+#include <mpi.h>
+#include <pthread.h>
+/* for definition of CHECK_EQ*/
+#include "glog/logging.h"
+#include "nccl/src/nccl.h"
+
+#ifndef N_PROC_PER_GROUP
+#define N_PROC_PER_GROUP 1
+#endif
+
+#ifndef N_MACHINE_PER_GROUP
+#define N_MACHINE_PER_GROUP 1 	
+#endif
+
+#ifndef N_PROC_PER_MACHINE
+#define N_PROC_PER_MACHINE 1
+#endif
+
+#ifndef N_DEVICE_PER_PROC
+#define N_DEVICE_PER_PROC  2
+#endif
+
+#ifndef GROUP_ROOT_COMM_ID
+#define GROUP_ROOT_COMM_ID 0
+#endif
+
+#ifndef NON_GROUP_ROOT_COMM_ID
+#define NON_GROUP_ROOT_COMM_ID 0
+#endif
+
+#ifndef CLIQUE_ROOT_RANK
+#define CLIQUE_ROOT_RANK 0
+#endif
+
+#define CUDA_CHECK(condition) \
+  /* Code block avoids redefinition of cudaError_t error */ \
+  do { \
+    cudaError_t error = condition; \
+    CHECK_EQ(error, cudaSuccess) << " " << cudaGetErrorString(error); \
+  } while (0)
+
+
+/* distinguish message within sync group or asynchronized inter-groups */
+enum MsgType { ASYNC_MSG = 0, SYNC_MSG = 1};
+
+/* Compile time mapping from typename Dtype to ncclDataType_t */
+template <typename Dtype>
+struct DtypeToNCCLDtype {};
+
+template<> struct DtypeToNCCLDtype<float> { 
+  const static ncclDataType_t type = ncclFloat; 
+};
+
+template<> struct DtypeToNCCLDtype<double> { 
+  const static ncclDataType_t type = ncclDouble; 
+};
+
+void GetGpuIds(std::vector<int>& gpu_ids);
+
+
+/* Compile time mapping from typename Dtype to MPI_Datatype*/
+template <typename Dtype>
+struct DtypeToMPIDtype {};
+
+template<> struct DtypeToMPIDtype<float> {
+  const static MPI_Datatype type = MPI_FLOAT;
+};
+template<> struct DtypeToMPIDtype<double> {
+  const static MPI_Datatype type = MPI_DOUBLE;
+};
+
+
+#endif // end of COMM_UTILS_HPP_
